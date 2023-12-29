9255c8343fec183818e3589ba07d6cc3b636e31d
Luan Mark <luan.mark13@gmail.com>
2023-12-29 12:31:30 -0300

    HEY-2: installing larastan, setting up and fixing initial errors

--- app/View/Components/AppLayout.php
+++ app/View/Components/AppLayout.php
@@ -3,14 +3,14 @@
 namespace App\View\Components;
 
 use Illuminate\View\Component;
-use Illuminate\View\View;
+use Illuminate\Contracts\View\View as ViewContract;
 
 class AppLayout extends Component
 {
     /**
      * Get the view / contents that represents the component.
      */
-    public function render(): View
+    public function render(): ViewContract
     {
         return view('layouts.app');
     }
