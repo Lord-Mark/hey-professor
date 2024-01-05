be663834138b1dca50014170b2fde1f770b95dd2
Luan Mark <luan.mark13@gmail.com>
2024-01-05 17:54:16 -0300

    hey-4: adding like and dislike button on the view, todo: route to dislike

--- resources/views/components/question.blade.php
+++ resources/views/components/question.blade.php
@@ -1,4 +1,22 @@
 @props(['question'])
-<div class="rounded dark:bg-gray-800/50 dark:text-gray-400 p-2 shadow shadow-blue-500/20">
-    {{ $question->question }}
+<div class="
+        rounded dark:bg-gray-800/50 dark:text-gray-400 p-3
+        shadow shadow-blue-500/20 flex justify-between items-center
+    ">
+    <span>{{ $question->question }}</span>
+    <div class="flex space-x-2.5">
+        <x-form :action="route('question.like', $question)">
+            <button type="submit" class="flex items-center space-x-1">
+                <span>{{$question->likes}}</span>
+                <x-icons.thumbs-up class="w-5 h-5 text-sky-600/80 hover:text-sky-500 cursor-pointer"/>
+            </button>
+        </x-form>
+
+        <x-form :action="route('question.like', $question)">
+            <button type="submit" class="flex items-center space-x-1">
+                <span>{{$question->dislikes}}</span>
+                <x-icons.thumbs-down class="w-5 h-5 text-red-700 hover:text-red-800 cursor-pointer"/>
+            </button>
+        </x-form>
+    </div>
 </div>
