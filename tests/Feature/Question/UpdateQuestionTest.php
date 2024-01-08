1fb7d34716197851c8a5a23d8bced6e81337c8f6
Luan Mark <luan.mark13@gmail.com>
2024-01-08 17:47:55 -0300

    hey-6: it should be sure that only draft questions can be updated

--- tests/Feature/Question/UpdateQuestionTest.php
+++ tests/Feature/Question/UpdateQuestionTest.php
@@ -23,3 +23,22 @@ it('should be able to update a question', function () {
     expect($question->getAttribute('question'))->toBe($newQuestionMsg);
 
 });
+
+it('should be sure that only draft questions can be edited', function () {
+    // Arrange
+    $user             = factoryNewUser();
+    $draftQuestion    = Question::factory()->for($user)->create(['draft' => true]);
+    $notDraftQuestion = Question::factory()->for($user)->create(['draft' => false]);
+
+    $questionMsg = 'New question?';
+
+    actingAs($user);
+
+    // Act
+    $draftRequest    = put(route('question.update', $draftQuestion), ['question' => $questionMsg]);
+    $notDraftRequest = put(route('question.update', $notDraftQuestion), ['question' => $questionMsg]);
+
+    // Assert
+    $draftRequest->assertRedirect();
+    $notDraftRequest->assertForbidden();
+});
